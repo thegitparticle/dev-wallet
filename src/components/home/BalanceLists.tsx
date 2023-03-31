d1537664dd3e8aa095d71e9a53878bb0b15c86e1
thegitparticle <maddamsettysanjit@gmail.com>
2023-03-31 16:43:47 +0530

    add: balanceLists to home screen - tabs view changed

--- src/components/home/BalanceLists.tsx
+++ src/components/home/BalanceLists.tsx
@@ -1,13 +1,15 @@
 import { Tabs, H5, XStack } from "tamagui";
 import { Text } from "dripsy";
-import { Dimensions, ScrollView } from "react-native";
+import { Dimensions, ScrollView, useWindowDimensions } from "react-native";
 import { useLiveWalletsState } from "../../state/liveWalletsState";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { goerliAlchemy, mumbaiAlchemy } from "../../utils/alchemyConfig";
 import { ethers } from "ethers";
 import BalanceList from "../balances/BalanceList";
 import { useGoerliBalances } from "../../hooks/useGoerliBalances";
 import { useMumbaiBalances } from "../../hooks/useMumbaiBalances";
+import React from "react";
+import { TabView, SceneMap } from "react-native-tab-view";
 
 const dimensions = {
 	fullHeight: Dimensions.get("window").height,
@@ -31,88 +33,103 @@ export default function BalanceLists() {
 
 	const currentWallet = liveWalletsState.wallets[0];
 
-	function GoerliBalances() {
-		const { loading, balances } = useGoerliBalances(
-			// "0x1239b15ECE331E2eE767710a1953D889B59Ae84c"
-			currentWallet.address
-		);
+	const GoerliBalances = useMemo(
+		() =>
+			function Goerli() {
+				// const { loading, balances } = useGoerliBalances(
+				// 	// "0x1239b15ECE331E2eE767710a1953D889B59Ae84c"
+				// 	// currentWallet.address
+				// 	""
+				// );
 
-		if (loading)
-			return (
-				<Text
-					variant="text.heading_large"
-					sx={{ color: "light", my: "$2" }}
-					ellipsizeMode="tail"
-				>
-					loading ...
-				</Text>
-			);
+				// if (loading)
+				// 	return (
+				// 		<Text
+				// 			variant="text.heading_large"
+				// 			sx={{ color: "light", my: "$2" }}
+				// 			ellipsizeMode="tail"
+				// 		>
+				// 			loading ...
+				// 		</Text>
+				// 	);
 
-		return <BalanceList balanceList={balances} />;
-	}
+				// return <BalanceList balanceList={balances} />;
 
-	function MumbaiBalances() {
-		const { loading, balances } = useMumbaiBalances(
-			// "0x1239b15ECE331E2eE767710a1953D889B59Ae84c"
-			currentWallet.address
-		);
+				useEffect(() => {
+					console.log("goerli balances");
+				}, []);
 
-		if (loading)
-			return (
-				<Text
-					variant="text.heading_large"
-					sx={{ color: "light", my: "$2" }}
-					ellipsizeMode="tail"
-				>
-					loading ...
-				</Text>
-			);
+				return (
+					<Text
+						variant="text.heading_large"
+						sx={{ color: "light", my: "$2" }}
+						ellipsizeMode="tail"
+					>
+						goerli balances
+					</Text>
+				);
+			},
+		[]
+	);
 
-		return <BalanceList balanceList={balances} />;
-	}
+	const MumbaiBalances = useMemo(
+		() =>
+			function Mumbai() {
+				// const { loading, balances } = useMumbaiBalances(
+				// 	// "0x1239b15ECE331E2eE767710a1953D889B59Ae84c"
+				// 	// currentWallet.address
+				// 	""
+				// );
+
+				// if (loading)
+				// 	return (
+				// 		<Text
+				// 			variant="text.heading_large"
+				// 			sx={{ color: "light", my: "$2" }}
+				// 			ellipsizeMode="tail"
+				// 		>
+				// 			loading ...
+				// 		</Text>
+				// 	);
+
+				// return <BalanceList balanceList={balances} />;
+
+				useEffect(() => {
+					console.log("mumbai balances");
+				}, [currentWallet.address]);
+
+				return (
+					<Text
+						variant="text.heading_large"
+						sx={{ color: "light", my: "$2" }}
+						ellipsizeMode="tail"
+					>
+						mumbai balances
+					</Text>
+				);
+			},
+		[]
+	);
+
+	const renderScene = SceneMap({
+		goerli: GoerliBalances,
+		mumbai: MumbaiBalances,
+	});
+
+	const layout = useWindowDimensions();
+
+	const [index, setIndex] = React.useState(0);
+	const [routes] = React.useState([
+		{ key: "goerli", title: "Goerli" },
+		{ key: "mumbai", title: "Mumbai" },
+	]);
 
 	return (
-		<Tabs
-			defaultValue="tab1"
-			width={dimensions.fullWidth - 40}
-			orientation="horizontal"
-			style={{ flexDirection: "column" }}
-		>
-			<Tabs.List space>
-				{supportedNetworks.map((network) => {
-					return (
-						<Tabs.Trigger value={network.name.toLocaleLowerCase()}>
-							<Text
-								variant="text.heading_large"
-								sx={{ color: "light", my: "$2" }}
-								ellipsizeMode="tail"
-							>
-								{network.name}
-							</Text>
-						</Tabs.Trigger>
-					);
-				})}
-			</Tabs.List>
-
-			<Tabs.Content value="goerli">
-				<ScrollView>
-					<GoerliBalances />
-				</ScrollView>
-			</Tabs.Content>
-			<Tabs.Content value="mumbai">
-				<ScrollView>
-					<MumbaiBalances />
-				</ScrollView>
-			</Tabs.Content>
-			<Tabs.Content value="mainnet">
-				<Text
-					variant="text.heading_large"
-					sx={{ color: "light", my: "$2" }}
-					ellipsizeMode="tail"
-				>
-					Tab 3
-				</Text>
-			</Tabs.Content>
-		</Tabs>
+		<TabView
+			navigationState={{ index, routes }}
+			renderScene={renderScene}
+			onIndexChange={setIndex}
+			initialLayout={{ width: layout.width - 40 }}
+		/>
 	);
 }
